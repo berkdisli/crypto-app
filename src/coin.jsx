273bb63bea448f21f357a6b84783335c8f3d29e3
Berk Disli <berkdisli@yahoo.com>
2022-08-24 09:34:14 +0200

    some small changes

--- src/coin.jsx
+++ src/coin.jsx
@@ -1,25 +1,29 @@
 import React from "react";
-import "./coin.css";
+import "./Coin.css";
 
-const coin = ({ image, name, price, volume, pricechange, marketcap }) => {
-  return;
-  <div className="coin-container">
-    <div className="coin-row">
-      <div className="coin">
-        <img src="{image}" alt="crypto" />
-        <h1>{name}</h1>
-        <p className="coin-symbol"></p>
+const Coin = ({ image, name, price, pricechange, marketcap }) => {
+  return (
+    <div className="coin-container">
+      <div className="coin-row">
+        <div className="coin">
+          <img src={image} alt="crypto" />
+          <h1>{name}</h1>
+          <p className="coin-symbol"></p>
+        </div>
+        <div className="coin-data">
+          <p className="coin-price">Rs.{price}</p>
+          {pricechange < 0 ? (
+            <p className="coin-percent red">{pricechange.toFixed(2)}%</p>
+          ) : (
+            <p className="coin-percent green">{pricechange.toFixed(2)}%</p>
+          )}
+          <p className="coin-marketcap">
+            Mkt Cap: Rs.{marketcap.toLocaleString()}
+          </p>
+        </div>
       </div>
-      <div className="coin-data"></div>
-      <p className="coin-price">Rs.{price}</p>
-      {pricechange < 0 ? (
-        <p className="coin-percent red">{pricechange.toFixed(2)}%</p>
-      ) : (
-        <p className="coin-percent-green">{pricechange.toFixed(2)}%</p>
-      )}
-      <p className="coin-marketcap">Mkt Cap: Rs.{marketcap.toLocaleString()}</p>
     </div>
-  </div>;
+  );
 };
 
-export default coin;
+export default Coin;
