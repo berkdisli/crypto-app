6554d5d65b177c8aa81795a51bcda3be50100892
Berk Disli <berkdisli@yahoo.com>
2022-09-02 09:44:53 +0200

    some small additions and changes on coin

--- src/coin.jsx
+++ src/coin.jsx
@@ -1,24 +1,25 @@
 import React from "react";
-// import "./Coin.css";
+import "./coin.css";
 
-const Coin = ({ image, name, price, pricechange, marketcap }) => {
+const Coin = ({ image, name, symbol, price, priceChange, marketCap }) => {
   return (
     <div className="coin-container">
       <div className="coin-row">
         <div className="coin">
           <img src={image} alt="crypto" />
           <h1>{name}</h1>
-          <p className="coin-symbol"></p>
+          <p className="coin-symbol">{symbol}</p>
         </div>
         <div className="coin-data">
-          <p className="coin-price">Rs.{price}</p>
-          {pricechange < 0 ? (
-            <p className="coin-percent red">{pricechange.toFixed(2)}%</p>
+          <p className="coin-price"> Price: ${price}</p>
+          {priceChange < 0 ? (
+            //change for 24 hours
+            <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
           ) : (
-            <p className="coin-percent green">{pricechange.toFixed(2)}%</p>
+            <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
           )}
           <p className="coin-marketcap">
-            Mkt Cap: Rs.{marketcap.toLocaleString()}
+            Market Cap: ${marketCap.toLocaleString()}
           </p>
         </div>
       </div>
